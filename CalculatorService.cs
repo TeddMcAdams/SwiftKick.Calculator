b4dbca70abcef79c3fab4d73d4e95f20186e58ec
Kevin Griffin <kevin@kevgriffin.com>
2018-09-17 12:19:29 -0400

    init

---
+++ CalculatorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwiftKick.Calculator
+{
+    public class CalculatorService
+    {
+        public int Add(int a, int b) => a + b;
+        public int Substract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+        public int Divide(int a, int b) => a / b;
+    }
+}
